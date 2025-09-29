a289d06ff22d2b36de3184da7c645f057e5ffc01
AlexisJ16 <114438880+AlexisJ16@users.noreply.github.com>
2025-09-29 18:52:35 -0500

    Implementa despliegue completo de Azure Function con Terraform

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,14 @@
-output "url" {
+output "function_invocation_url" {
+  description = "URL para invocar la función HTTP desplegada."
   value       = azurerm_function_app_function.faf.invocation_url
-  sensitive   = false
-  description = "description"
+}
+
+output "resource_group_name" {
+  description = "Nombre del resource group creado."
+  value       = azurerm_resource_group.rg.name
+}
+
+output "function_app_name" {
+  description = "Nombre de la Function App creada."
+  value       = azurerm_windows_function_app.wfa.name
 }
